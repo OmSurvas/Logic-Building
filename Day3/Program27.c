993fef45ce83d91c63a98ff10579150554cb42f4
Om Survase111 <omsurvase809@gmail.com>
2025-10-23 23:26:23 +0530

    Third Commit

---
+++ Day3/Program27.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+// Dynamic function
+void Display(int iFrequency)
+{
+    int iCnt = 0;
+    // Filter
+    if(iFrequency < 0)
+    {
+        printf("Invalid input\n");
+        return;
+    }
+    for (iCnt = 1; iCnt <= iFrequency; iCnt++)
+    {
+        printf("Jay Ganesh...\n");
+    }
+    
+}
+// It should be int main
+int main()
+{
+    int iCount = 0;
+    printf("Enter the frequency : \n");
+    scanf("%d",&iCount);
+    Display(iCount); 
+    return 0;
+}
---
+++ Day3/Program39.c
@@ -0,0 +1,20 @@
+// 1 2 3 4 5 
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = iNo; iCnt >=0; iCnt--)
+    {
+        printf("%d\t",iCnt);    
+    }
+}
+int main()
+{ 
+    int iValue = 0;
+
+    printf("please enter frequency : \n");
+    scanf("%d",&iValue);
+    Display(iValue);
+    return 0;
+}
