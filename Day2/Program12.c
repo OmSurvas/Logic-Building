92957bbf4382887cffa4fb3b33a7b40b22f81cc7
Om Survase111 <omsurvase809@gmail.com>
2025-10-23 23:25:00 +0530

    Second Commit

---
+++ Day2/Program12.c
@@ -0,0 +1,38 @@
+/*
+    START
+        Accept Number And Store as no
+        Divide no by 2
+        if the remainder is 0
+            then display as Even
+        otherwise
+            display as odd
+    STOP
+*/
+#include<stdio.h>
+
+void CheckEvenOdd(int iNo)
+    {
+        int iRem = 0;
+
+        iRem = iNo % 2;
+
+        if(iRem == 0)
+        {
+            printf("It is Even Number\n");
+        }
+        else
+        {
+            printf("It is Odd Number\n");
+        }
+        
+    }
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number:");
+    scanf("%d",&iValue);
+
+    CheckEvenOdd(iValue);
+    return 0;
+}
